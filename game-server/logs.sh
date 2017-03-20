388e5bf78b018191e327bd3a64e74694315bd269
jin.li <funplus@jinlideMacBook-Pro.local>
2017-03-20 14:31:16 +0800

    server config

---
+++ game-server/logs.sh
@@ -0,0 +1,19 @@
+mkdir logs
+cd logs
+echo "" > con-log-gate-server-1.log
+echo "" > rpc-log-gate-server-1.log
+echo "" > forward-log-gate-server-1.log
+echo "" > rpc-debug-gate-server-1.log
+echo "" > crash.log
+echo "" > admin.log
+echo "" > pomelo-gate-server-1.log
+echo "" > pomelo-admin.log
+echo "" > pomelo-rpc-gate-server-1.log
+echo "" > con-log-connector-server-1.log
+echo "" > rpc-log-connector-server-1.log
+echo "" > forward-log-connector-server-1.log
+echo "" > rpc-debug-connector-server-1.log
+echo "" > crash.log
+echo "" > admin.log
+echo "" > pomelo-connector-server-1.log
+echo "" > pomelo-admin.log
